7541ed7e5a56fb344467c9baf05be42c4f6a5b51
Seonghyeon Kim <artoria@artoria.us>
2019-10-03 18:22:56 +0900

    ADD: school search view

--- avengers/presentation/blueprint.py
+++ avengers/presentation/blueprint.py
@@ -3,3 +3,5 @@ from sanic import Blueprint
 from avengers.presentation import view
 
 bp = Blueprint(__name__.split(".")[0], url_prefix="/api/v1")
+
+bp.add_route(view.SchoolSearchView.as_view(), "/school/search")
