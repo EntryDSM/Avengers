dd9223fb599a8c89ba635f2c226726d818edabbf
Seonghyeon Kim <artoria@artoria.us>
2019-10-06 17:12:34 +0900

    FIX: date format

--- avengers/presentation/schema/common.py
+++ avengers/presentation/schema/common.py
@@ -6,5 +6,5 @@ from marshmallow.fields import Date as BaseDate
 class Date(BaseDate):
     def _deserialize(self, value, attr, data, **kwargs):
         if isinstance(value, date):
-            return value
+            return str(value)
         return super()._deserialize(value, attr, data)
